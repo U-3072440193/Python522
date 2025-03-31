1acc9a48f2d16e769ff786612094fb56986f456c
Q Antonivs <vergvoktre@gmail.com>
2025-03-31 11:27:59 +0300

    dz20.py added

---
+++ Home Work/dz20.py
@@ -0,0 +1,23 @@
+import os
+
+
+print("Домашнее задание")
+empty_files_dir = "Work/empty_files"
+os.makedirs(empty_files_dir, exist_ok=True)
+
+
+def empty_file_search(directory):
+    for root, directories, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if os.path.exists(file_path):
+                size = os.path.getsize(file_path)
+                if size == 0:
+                    new_file_path = os.path.join(empty_files_dir, file)
+                    os.rename(file_path, new_file_path)
+                    print(f"Файл {file} перемещён из {file_path} в {new_file_path}")
+                else:
+                    print(f"{file_path} - {size} bytes")
+
+
+empty_file_search("Work")
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import os
 
-word = "Minas"  # Искомое слово
-with open(r"C:\Program Files (x86)\R.G. Catalyst\Medieval 2 - Total War Kingdoms\mods\Third_Age_3\data\world\maps\base\descr_regions.txt", "r", encoding="utf-8") as f:
-    for line in f:
-        if word in line:
-            print(line.strip())  # Вывод найденной строк
+dirs = [r'Work\F1', r'Work\F2\F21']
+
+for d in dirs:
+    os.makedirs(d)
+
+files = {
+    'Work': ['w.txt'],
+    r'Work\F1': ['f11.txt', 'f12.txt', 'f13.txt'],
+    r'Work\F2\F21': ['f211.txt', 'f212.txt']
+}
+
+for key, value in files.items():
+    for file in value:
+        file_path = os.path.join(key, file)
+        open(file_path, 'w').close()
